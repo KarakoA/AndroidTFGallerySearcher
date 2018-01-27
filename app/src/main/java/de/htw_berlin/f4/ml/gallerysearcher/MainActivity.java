016799892f88044964203a71248b881d564e7b62
Anton K <s0553324@htw-berlin.de>
2018-01-27 11:15:04 +0100

    add second empty activity and navigation between them

--- app/src/main/java/de/htw_berlin/f4/ml/gallerysearcher/MainActivity.java
+++ app/src/main/java/de/htw_berlin/f4/ml/gallerysearcher/MainActivity.java
@@ -1,7 +1,9 @@
 package de.htw_berlin.f4.ml.gallerysearcher;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onNavigateToGalleryClicked(View view) {
+        //create a new intent to navigate to the gallery activity
+        Intent intent = new Intent(this, GalleryActivity.class);
+        startActivity(intent);
     }
 }
